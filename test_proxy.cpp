17b3f1cd06716a70b478ef7e1f83bc38a45e03a3
kn201905 <zz2018-dr@yahoo.co.jp>
2022-07-01 01:48:28 +0900

    Update test_proxy.cpp

--- test_proxy.cpp
+++ test_proxy.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/epoll.h>
+#include <arpa/inet.h>
 
 enum
 {
@@ -75,9 +76,9 @@ int main()
 	}
 	std::cout << "--- fd_epoll を生成しました。" << std::endl;
 
-	// fd_epoll に fd_sock_listner を登録
+	// fd_sock_listner を fd_epoll に登録する
 	{
-		epoll_event e;
+		epoll_event e = {};
 		e.events = EPOLLIN;
 		e.data.ptr = NULL;	// 本来は必要なアドレスを設定する
 	
@@ -92,30 +93,134 @@ int main()
 	}
 	
 	// 接続待機
-	epoll_event ary_events[EN_max_pcs_epoll_events];
-	const int ret_epoll_wait = epoll_wait(fd_epoll, ary_events, EN_max_pcs_epoll_events
-		, -1	// timeout（ミリ秒）/ -1 を指定すると無制限に待機する
-	);
-	std::cout << "--- epoll_wait から抜けました。" << std::endl;
+	{
+		epoll_event ary_events[EN_max_pcs_epoll_events];
+		const int ret_epoll_accept = epoll_wait(fd_epoll, ary_events, EN_max_pcs_epoll_events
+			, -1	// timeout（ミリ秒）/ -1 を指定すると無制限に待機する
+		);
+		std::cout << "--- epoll_wait（for accept）から抜けました。" << std::endl;
+
+		if (ret_epoll_accept != 1)
+		{
+			std::cout << "!!! ret_epoll_accept != 1 となりました。" << std::endl;
+			close(fd_sock_listner);
+			close(fd_epoll);
+			return -1;
+		}
+//		std::cout << "--- ret_epoll_accept == 1 となりました。" << std::endl;
+	}
+
+	// int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+	const int fd_sock_client = accept(fd_sock_listner, NULL, NULL);
+	if (fd_sock_client < 0)
+	{
+		std::cout << "!!! accept() に失敗しました。" << std::endl;
+		close(fd_sock_listner);
+		close(fd_epoll);
+		return -1;
+	}
+	std::cout << "--- accept() に成功しました。" << std::endl;
+
+	// 接続相手の情報を表示する
+	{
+		sockaddr_in6 addr_client;
+		socklen_t addrlen = sizeof(addr_client);
+		if (getpeername(fd_sock_client, (sockaddr*)&addr_client, &addrlen) < 0)
+		{
+			std::cerr << "!!! fd_sock_client -> getpeername() が失敗しました。" << std::endl;
+			close(fd_sock_listner);
+			close(fd_epoll);
+			close(fd_sock_client);
+			return -1;
+		}
+		std::cout << "--- getpeername() に成功しました。" << std::endl;
+
+		char str_client_ip_addr[INET6_ADDRSTRLEN];  // INET6_ADDRSTRLEN = 46
+		if (inet_ntop(AF_INET6, &addr_client.sin6_addr, str_client_ip_addr, sizeof(str_client_ip_addr)) == NULL)
+		{
+			std::cerr << "!!! fd_sock_client -> inet_ntop() が失敗しました。" << std::endl;
+			close(fd_sock_listner);
+			close(fd_epoll);
+			close(fd_sock_client);
+			return -1;
+		}
+		std::cout << "    client address -> " << str_client_ip_addr << std::endl;
+		std::cout << "    client port -> " << addr_client.sin6_port << std::endl;
+	}
+
+	// fd_sock_client を fd_epoll に登録する
+	{
+		epoll_event e = {};
+		e.events = EPOLLIN;
+		e.data.ptr = NULL;	// 本来は必要なアドレスを設定する
+	
+		if (epoll_ctl(fd_epoll, EPOLL_CTL_ADD, fd_sock_client, &e) < 0)
+		{
+			std::cout << "!!! fd_sock_client -> epoll_ctl() が失敗しました。" << std::endl;
+			close(fd_sock_listner);
+			close(fd_epoll);
+			close(fd_sock_client);
+			return -1;
+		}
+		std::cout << "--- fd_epoll に fd_sock_client を登録しました。" << std::endl;
+	}
+
+	// read 待機
+	{
+		epoll_event ary_events[EN_max_pcs_epoll_events];
+		const int ret_epoll_read = epoll_wait(fd_epoll, ary_events, EN_max_pcs_epoll_events
+			, -1	// timeout（ミリ秒）/ -1 を指定すると無制限に待機する
+		);
+		std::cout << "--- epoll_wait（for read）から抜けました。" << std::endl;
+
+		if (ret_epoll_read != 1)
+		{
+			std::cout << "!!! ret_epoll_read != 1 となりました。" << std::endl;
+			close(fd_sock_listner);
+			close(fd_epoll);
+			close(fd_sock_client);
+			return -1;
+		}
+//		std::cout << "--- ret_epoll_wait == 1 となりました。" << std::endl;
+		// 本来は、epoll_wait() の戻り値の対象を確認する必要あり
+	}
+
+	// read
+	{
+		unsigned char buf_read[4000];
+		ssize_t bytes_read = read(fd_sock_client, buf_read, 4000);
+
+		if (bytes_read < 0 || bytes_read == 4000)
+		{
+			std::cout << "!!! read() に失敗しました。" << std::endl;
+			close(fd_sock_listner);
+			close(fd_epoll);
+			close(fd_sock_client);
+			return -1;
+		}
+		std::cout << "--- read() に成功しました。bytes -> " << bytes_read << std::endl;
+
+		buf_read[bytes_read] = 0;
+		std::string str_read{ (const char*)buf_read };
+		std::cout << "--- 読み込み内容" << std::endl;
+		std::cout << str_read << std::endl;
+	}
 
 	// 終了処理
 	if (close(fd_sock_listner) < 0)
-	{
-		std::cout << "!!! リスナ用のソケットの close に失敗しました。" << std::endl;
-	}
+	{ std::cout << "!!! fd_sock_listner の close に失敗しました。" << std::endl; }
 	else
-	{
-		std::cout << "--- fd_sock_listner を close しました。" << std::endl;
-	}
+	{ std::cout << "--- fd_sock_listner を close しました。" << std::endl; }
 
 	if (close(fd_epoll) < 0)
-	{
-		std::cout << "!!! fd_epoll の close に失敗しました。" << std::endl;
-	}
+	{ std::cout << "!!! fd_epoll の close に失敗しました。" << std::endl; }
 	else
-	{
-		std::cout << "--- fd_epoll を close しました。" << std::endl;
-	}
+	{ std::cout << "--- fd_epoll を close しました。" << std::endl; }
+
+	if (close(fd_sock_client) < 0)
+	{ std::cout << "!!! fd_sock_client の close に失敗しました。" << std::endl; }
+	else
+	{ std::cout << "--- fd_sock_client を close しました。" << std::endl; }
 
 	return 0;
 }
